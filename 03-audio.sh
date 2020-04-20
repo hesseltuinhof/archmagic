fcb47a61c39fff359db1a7bd83d8786c180f532c
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2020-04-21 00:15:55 +0200

    Add package list for audio (alsa, pulse, pipewire)

---
+++ 03-audio.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+PKGS=(
+  alsa-utils            # amixer and alsamixer
+  pulseaudio            # sound server
+  pulseaudio-alsa       # ALSA support
+  pulseaudio-bluetooth  # bluetooth support
+  pavucontrol           # volume control GUI
+  pipewire              # future sound server
+  pipewire-pulse        # PulseAudio support
+)
