582dec5603aa665a4f6c1c6768fa7942ef1caf81
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2020-04-21 00:15:19 +0200

    Add package list for networking (wifi, bluetooth)

---
+++ 02-networking.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+PKGS=(
+  # bluetooth
+  bluez             # bluetooth daemon
+  bluez-utils       # bluetoothctl utility
+  blueman           # bluetooth GUI utility
+  
+  # networkmanager
+  networkmanager    # network utility
+  wireguard-tools   # VPN
+)
