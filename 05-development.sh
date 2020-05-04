11de46a86d7f2edbd87f61a61b063ec5155d3484
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2020-05-04 23:38:39 +0100

    Add package list for development

---
+++ 05-development.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+PKGS=(
+  # Languages
+  julia
+  python
+  go
+  go-tools
+  r
+  maxima
+
+  # tools
+  git             # version control system
+  neovim          # fork of vim
+  ninja           # fast build system
+  meson           # build system
+  cmake           # make system
+  scdoc           # generate manual pages
+
+  # other   
+  hub             # CLI for Github
+  hugo            # static site generator
+)
