c851e674af69d18085650a54666418bc8cf0a230
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2020-05-04 23:38:21 +0100

    Add package list for security

---
+++ 04-security.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+PKGS=(
+  openssh         # remote login
+  gnupg           # OpenPGP
+  gopass          # password manager (pass-compatible)
+  gnome-keyring   # store encryption keys
+  seahorse        # GUI for gnome-keyring
+)
