f6e9f14e4f682cec986b0af6614433b91d2eb14e
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2020-06-26 00:30:26 +0100

    Add configuration for security

---
+++ 04-security-cfg.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+echo ":: Configuring security..."
+
+echo " Enabling PAM support for Gnome keyring."
+sed -i "/^auth.*include.*system-local-login$/a auth       optional     pam_gnome_keyring.so" /etc/pam.d/login
+sed -i "/^session.*include.*system-local-login$/a session    optional     pam_gnome_keyring.so auto_start" /etc/pam.d/login
