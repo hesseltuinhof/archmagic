853d89fc0619dd82f0aa0a49de315eb17c6b5c57
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2021-03-04 21:56:37 +0000

    Update Arch mirrorlist URI

--- utils/mirrorlist.sh
+++ utils/mirrorlist.sh
@@ -2,7 +2,7 @@
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s \
-  "https://www.archlinux.org/mirrorlist/?country=AT&country=IE&country=NL&country=CH&country=GB&protocol=https&ip_version=4" \
+  "https://archlinux.org/mirrorlist/?country=DE&country=NL&country=NL&protocol=https&use_mirror_status=on" \
   | sed -e "s/^#Server/Server/" -e "/^#/d" \
   > /etc/pacman.d/mirrorlist
 pacman -Syyu
